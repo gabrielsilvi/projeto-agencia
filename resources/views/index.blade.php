71d125d735c60747c7eb0f7672da38e6394dda87
gabrielsilvi <gabrielsv3000@hotmail.com>
2020-12-12 21:58:16 -0300

    editado detalhe na view index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,27 +1,24 @@
 @extends('layouts.app2')
 
 @section('content')
-
-    <h1>Mais Vistos</h1>
-
-
-    <div class="container row">
-
-        @foreach ($pacotes as $pacote)
+    <div class="container">
+        <h1>Mais Vistos</h1>
+        <div class="row">
+            @foreach ($pacotes as $pacote)
             <div class="col-sm" style="margin-left: 10px">
                 <div class="card " style="border-radius: 22px;">
                     <img class="card-img-top" style="border-radius: 22px 22px 0px 0px;"
-                        src="{{ url('assets/img/fundo.jpg') }}" alt="Card image cap">
+                    src="{{ url('assets/img/fundo.jpg') }}" alt="Card image cap">
                     <div class="card-body" style="padding: 0.5em">
                         <p>Pacotes Para</p>
                         @foreach ($pontos as $ponto)
                             @if ($ponto->cidade->id == $pacote->pontoturistico->cidade_id )
-                                @if ($anterior == $ponto->cidade->nomeCidade)
-                                    @break
-                                @else
-                                <?php $anterior=$ponto->cidade->nomeCidade ?>
-                                    {{ $ponto->cidade->nomeCidade }}
-                                @endif
+                            @if ($anterior == $ponto->cidade->nomeCidade)
+                                @break
+                            @else
+                            <?php $anterior=$ponto->cidade->nomeCidade ?>
+                                {{ $ponto->cidade->nomeCidade }}
+                            @endif
                             @endif      
                         @endforeach
                         <h5 class="card-title">{{ $pacote->pontoturistico->nomePontoTuristico }}</h5>
@@ -30,8 +27,8 @@
                     </div>
                 </div>
             </div>
-        @endforeach
-
+            @endforeach
+        </div>
     </div>
     {{-- {!! $pontos->links() !!} --}}
 
